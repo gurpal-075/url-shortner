74aa1af36bce168c54cfdb1e204f85f644856682
gurpal-075 <gurpalsingh51696@gmail.com>
2025-10-25 18:47:21 +0530

    auth 6 commit

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,41 @@
 import express from "express";
-import {nanoid} from "nanoid";
-import dotenv from "dotenv";
-import connectDB from "./src/config/mongodb.config.js";
-import shortUrl from "./src/routes/shortUrl.routes.js"
+import {nanoid} from "nanoid"
+import dotenv from "dotenv"
+import connectDB from "./src/config/mongodb.config.js"
+import short_url from "./src/routes/shortUrl.routes.js"
+import user_routes from "./src/routes/user.routes.js"
 import auth_routes from "./src/routes/auth.routes.js"
 import { redirectFromShortUrl } from "./src/controller/shortUrl.controller.js";
 import { errorHandler } from "./src/utils/errorhandler.js";
-import cors from "cors";
-dotenv.config("./.env");
+import cors from "cors"
+import { attachUser } from "./src/utils/attachUser.js";
+import cookieParser from "cookie-parser"
 
-
-
+dotenv.config("./.env")
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', // your React app
+    credentials: true // 👈 this allows cookies to be sent
+}));
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
-app.use("/api/auth",auth_routes);
-app.use("/api/create",shortUrl);
+app.use(attachUser)
 
-app.use("/:id",redirectFromShortUrl)
+app.use("/api/user",user_routes)
+app.use("/api/auth",auth_routes)
+app.use("/api/create",short_url)
+app.get("/:id",redirectFromShortUrl)
 
 app.use(errorHandler)
 
-
-app.listen(5000, () =>{
-    connectDB();
-    console.log('Server is running on http://localhost:5000');
+app.listen(3000,()=>{
+    connectDB()
+    console.log("Server is running on http://localhost:3000");
 })
+
+// GET - Redirection 
--- backend/src/utils/errorhandler.js
+++ backend/src/utils/errorhandler.js
@@ -1,45 +1,50 @@
-export const errorHandler = (err, req, res, next) => {
-    if(err instanceof AppError){
-        return res.status(err.statusCode).json({
-            success: false,
-            message: err.message,
-        });
-    }
-    console.error(err);
-    res.status(500).json({
-        success: false,
-        message: err.message || "Internal Server Error",
+
+export const errorHandler = (err,req,res,next) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
     });
+  }
+
+  res.status(500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
 };
 
 export class AppError extends Error {
     statusCode;
     isOperational;
-
-    constructor(message, statusCode=500, isOperatonal = true){
-        super(message);
-        this.satusCode = statusCode;
-        this.isOperational = isOperatonal;
-        Error.captureStackTrace(this, this.constructor);
+  
+    constructor(message, statusCode = 500, isOperational = true) {
+      super(message);
+      this.statusCode = statusCode;
+      this.isOperational = isOperational;
+      Error.captureStackTrace(this, this.constructor);
     }
-}
-export class NotFoundError extends AppError {
-    constructor(message = "Resource Not Found"){
-        super(message, 404);
+  }
+  
+  export class NotFoundError extends AppError {
+    constructor(message = "Resource not found") {
+      super(message, 404);
     }
-}
-export class ConflictError extends AppError {
-    constructor(message = "Conflict occured"){
-        super(message, 409);
+  }
+  
+  export class ConflictError extends AppError {
+    constructor(message = "Conflict occurred") {
+      super(message, 409);
     }
-}
-export class BadRequestError extends AppError {
-    constructor(message = "bad request"){
-        super(message, 400);
+  }
+  
+  export class BadRequestError extends AppError {
+    constructor(message = "Bad request") {
+      super(message, 400);
     }
-}
-export class UnauthorizedError extends AppError {
-    constructor(message = "Unauthorized"){
-        super(message, 401);
+  }
+  
+  export class UnauthorizedError extends AppError {
+    constructor(message = "Unauthorized") {
+      super(message, 401);
     }
-}
+  }
